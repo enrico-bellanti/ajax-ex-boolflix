517a88b060842792d145866e72ce317c7d8eb6a3
Enrico Bellanti <enrico.bellanti@gmail.com>
2020-09-23 12:05:30 +0200

    Refactoring

--- js/script.js
+++ js/script.js
@@ -1,91 +1,125 @@
-$(document).ready(function() {
-  // $("#search_button").click(function() {
-  //   // cancello il risultato precedente
-  //   $(".movie_info").remove();
-  //   // prendo valore input
-  //   var searchMovie = $("#input_movie").val();
-  //   // stampa risultati
-  //   var totalPages = printResults(searchMovie);
-  //   printPageLabel(totalPages);
-  //   // ripulisci input
-  //   $("#input_movie").val("");
-  // });
-
-  $("#input_movie").keydown(function(event){
-    if (event.which == 13) {
+$(document).ready(function(){
+  var lastSearch = "";
+  // funzione cerca al click sul bottone
+  $(".search_button").click(function(){
+    // salvo il valore dell'input ricerca
+    var searchMovie = $(".search_input").val();
+    if (searchMovie != "") {
+      // salvo la ricerca in una variabile globale
+      lastSearch = $(".search_input").val();
+      // resetto la casella input
+      $(".search_input").val("");
       // cancello il risultato precedente
-      $(".movie_info").remove();
-      // prendo valore input
-      var searchMovie = $("#input_movie").val();
-      // stampa risultati
-      var totalPages = printResults(searchMovie);
-      console.log("click invio"+totalPages);
-
-      printPageLabel(7);
-      // ripulisci input
-      $("#input_movie").val("");
+      resetResult();
+      // stampo a schermo il nuovo risultato
+      getMovies(searchMovie);
     }
   });
 
+  // funzione cerca premendo invio
+  $(".search_input").keyup(function(){
+
+    if(event.which == 13){
+      // salvo il valore dell'input ricerca
+      var searchMovie = $(".search_input").val();
+      if (searchMovie != "") {
+        // salvo la ricerca in una variabile globale
+        lastSearch = $(".search_input").val();
+        // resetto la casella input
+        $(".search_input").val("");
+        // cancello il risultato precedente
+        resetResult();
+        // stampo a schermo il nuovo risultato
+        getMovies(searchMovie);
+      }
+    }
+
+  });
 
 
-
+  //click sul quadratino prende il valore di data-page e passiamo il valore alla funzione getMovies
+  $(document).on("click", ".page_numb", function(){
+    //seleziono con il this cioè quel quadratino che vado a selezionare
+    var actualPage = $(this).attr("data-page");
+    // cancello il risultato precedente
+    resetResult();
+    // printa a schermo risultato pagina selezionata
+    getMovies(lastSearch, actualPage);
+  });
 
 });
-// end document ready
 
+//Printa il risultato della risposta a schermo con i film e il numero di pagine
+function getMovies(searchMovies, page){
 
-// funzioni
+  var api_key = "e985f53e1e87b07c7fd1095468f025a0";
 
-// stampa a a schermo i risultati
-function printResults(searchKey) {
-  // var searchMovie = "star wars";
-  var source = $("#movie-template").html();
-  var movieTemplate = Handlebars.compile(source);
-
-  // chiamo il server per reperire le info film richiesto
   $.ajax(
-   {
-     "url":"https://api.themoviedb.org/3/search/movie",
-     "data":{
-       "api_key": "faa82c855e9e700015c133bf3942bd8f",
-       "language": "it-IT",
-       "query": searchKey,
-       "page": ""
-     },
-     "method":"GET",
-     "success":function (data) {
-       var movieResults = data.results;
-       var pageNumb = data.total_pages;
-       for (var i = 0; i < movieResults.length; i++) {
-         // compilo il template
-         var context = {
-           "title": movieResults[i].title,
-           "original_title": movieResults[i].original_title,
-           "original_language": movieResults[i].original_language,
-           "vote_average": movieResults[i].vote_average
-         };
-         var html = movieTemplate(context);
-         $(".movie_list").append(html);
-        }
-        console.log("numero pagine nella chiamata "+pageNumb);
-        return pageNumb;
-     },
-     "error":function (err) {
-       alert("E avvenuto un errore. "+ err);
-     }
-  });
+    {
+      "url": "https://api.themoviedb.org/3/search/movie",
+      "data": {
+        "api_key" : api_key,
+        "language": "it-IT",
+        "page": page,
+        "query": searchMovies
+      },
+      "method": "GET",
+      "success": function (data) {
+        renderMovies(data);
+        renderPages(data);
+      },
+      "error": function (err) {
+        alert("E' avvenuto un errore. " + err);
+      }
+    }
+  );
 }
 
-// funzione per stampare etichetta numero pagina
-function printPageLabel(pages) {
-  var source = $("#page-template").html();
+//stampo il numero di pagine della chiamata
+function renderPages(obj){
+  var totalPages = obj.total_pages;
+  //preparo il template
+  var source = $("#pages-template").html();
   var pageTemplate = Handlebars.compile(source);
-  for (var i = 0; i < pages; i++) {
+
+  //ciclo i numeri delle pagine prodotte dal risutato della query e compilo il context
+  for(var i = 1; i <= totalPages; i++){
     var context = {
-      "page_numb": i
-    };
+      "page": i
+    }
+
     var html = pageTemplate(context);
-    $(".nav_bar").append(html);
+    //stampo nel DOM
+    $(".movies-page-list").append(html);
   }
+}
+
+// resetto la casella di ricerca input
+function resetResult() {
+  $("#movies-list").html("");
+  $(".movies-page-list").html("");
+}
+
+// renderizza il template del film
+function renderMovies(obj) {
+  //preparo il template
+  var source = $("#movies-template").html();
+  var template = Handlebars.compile(source);
+  //array del risultato
+  var results = obj.results;
+
+
+  for(var i = 0; i < results.length; i++){
+
+    var context = {
+      "title": results[i].title,
+      "original_title": results[i].original_title,
+      "original_language" : results[i].original_language,
+      "vote_average": results[i].vote_average
+    };
+
+    var html = template(context);
+    $("#movies-list").append(html);
+  }
+
 }

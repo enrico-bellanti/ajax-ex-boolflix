321c4d9c5ec9eba37762f32d771b78b1bc20444d
Enrico Bellanti <enrico.bellanti@gmail.com>
2020-11-12 15:17:52 +0100

    end project

--- js/functions.js
+++ js/functions.js
@@ -14,3 +14,33 @@ function scrollHorizontal(container, list, elements, angles) {
     }
   });
 }
+
+// ISTRUZIONI E FUNZIONE PER RESIZE DIV CON MOUSE
+
+// <style>
+// #resizable { width: 200px; height: 150px; padding: 5px; }
+// #resizable h3 { text-align: center; margin: 0; }
+// </style>
+//
+// <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+// <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+//
+// <script>
+$( function() {
+  $( "#resizable" ).resizable({
+    maxHeight: 150,
+    maxWidth: 350,
+    minHeight: 150,
+    minWidth: 200,
+    handles: "e"
+  });
+} );
+// </script>
+//
+//
+// </head>
+// <body>
+//
+// <div id="resizable" class="ui-widget-content">
+// <h3 class="ui-widget-header">Resize larger / smaller</h3>
+// </div>
